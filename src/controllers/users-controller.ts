3c13eab7d981a10d915598b24ffbc0188bacbdd3
danilopinho75 <danilopinho11@gmail.com>
2025-12-27 09:00:43 -0300

    Início da configuração das controllers e routes

---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+
+
+class UsersController {
+  create(request: Request, response: Response) {
+    return response.json({ message: "ok" })
+  }
+}
+
+export { UsersController }
