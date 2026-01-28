f4be0723e7ee77ecd9c73be0d2429fbb506e05ae
danilopinho75 <danilopinho11@gmail.com>
2026-01-27 21:37:28 -0300

    Criando sessão do usuário

--- src/controllers/session-controller.ts
+++ src/controllers/session-controller.ts
@@ -1,7 +1,32 @@
 import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
+import { compare } from "bcrypt";
+import { z } from "zod";
 
 class SessionsController {
-  create(request: Request, response: Response) {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6)
+    })
+
+    const { email, password } = bodySchema.parse(request.body);
+
+    const user = await prisma.user.findFirst({
+      where: { email }
+    }) 
+
+    if(!user) {
+      throw new AppError("Email ou senha inválidos", 401);
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if(!passwordMatched) {
+      throw new AppError("Email ou senha inválidos", 401);
+    }
+
     return response.json({ message: "Sessão criada com sucesso!" });
   }
 }
