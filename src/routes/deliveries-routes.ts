184958090b5786f0e17c870448dd7e16c839b088
danilopinho75 <danilopinho11@gmail.com>
2026-02-14 11:30:55 -0300

    Listando pedidos

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -10,5 +10,6 @@ const deliveriesController = new DeliveriesController();
 deliveriesRoutes.use(ensureAuthenticated);
 deliveriesRoutes.use(verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesController.create);
+deliveriesRoutes.get("/", deliveriesController.index);
 
 export { deliveriesRoutes };
